0d1a9809b7c61d4d1ab4a330261b5ed8b7b8371d
seanflood <seanflood8848@gmail.com>
2022-03-05 15:28:52 -0500

    reset problems

--- js/app.js
+++ js/app.js
@@ -2,32 +2,29 @@
  * Project 4 - OOP Game App
  * app.js */
 
-let game 
+
 const startButton = document.getElementById('btn__reset')
 const keyButtons = document.getElementsByClassName('key')
-const keyIsh = document.getElementsByClassName('keyrow')
+const keyChildren = document.getElementsByClassName('keyrow')
 const keyRows = document.getElementById('qwerty')
 const phraseList = document.getElementById('phrase').firstElementChild;
 const overlay = document.getElementById("overlay")
 const singleButtons = document.querySelectorAll('button.key')
+const hearts = document.getElementsByClassName('tries');
+const scoreBoard = document.getElementById('scoreboard').firstElementChild; 
+const gameOverMessage = document.getElementById('game-over-message')
+const correctButton = document.querySelectorAll('button.chosen')
+const wrongButton = document.querySelectorAll('button.wrong')
+const buttons = document.querySelectorAll('button')
 
 
+ 
 
 
-
-
-//Working start button listener
-
-// startButton.addEventListener('click',  (e) => {
-//         if(e.target === startButton){
-//           keyRows.style.display = 'block';
-//           overlay.style.display = 'none'  
-//           console.log('shithouse');
-//         }
-//     })
 
 
 startButton.addEventListener('click',  function () {
+    
     keyRows.style.display = 'block';
     overlay.style.display = 'none';
     game = new Game();
